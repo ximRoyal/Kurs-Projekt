70943bbc4e1e9a4cd1a353a1f6ba81b1ebaef5cf
ebenebarde <eliasschaefer05@gmail.com>
2024-11-04 15:32:53 +0100

    new redirect uri

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ db.run(`ALTER TABLE users ADD COLUMN spotify_id TEXT UNIQUE`, (err) => {
 // Spotify API Konfiguration
 const client_id = '98621f9004dd40ddb39e47dab58dc971'; // Ersetze durch deine Spotify Client ID
 const client_secret = '1c3e31bb5fdf46a3b4939aee80a6b8cc'; // Ersetze durch deinen Spotify Client Secret
-const redirect_uri = 'http://localhost:3000/callback'; // Ersetze durch deine Redirect URI
+const redirect_uri = 'http://localhost:5000/callback'; // Ersetze durch deine Redirect URI
 
 // Hilfsfunktion zum Generieren eines zufälligen Strings
 function generateRandomString(length) {
