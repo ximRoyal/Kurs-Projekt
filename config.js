d4402f23dc260fb0c737e684ecc7946e0b989fcd
maxrmn <max.reimann.mr@gmail.com>
2024-11-11 11:13:43 +0100

    af

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 
 // config.js
 const config = {
-  client_id: 'your_client_id_here',
-  client_secret: 'your_client_secret_here',
-  redirect_uri: 'http://localhost:5000/callback'
+  client_id: '98621f9004dd40ddb39e47dab58dc971',
+  client_secret: '1c3e31bb5fdf46a3b4939aee80a6b8cc',
+  redirect_uri: 'http://localhost:5000/callback'  
 };
 
 module.exports = config;
